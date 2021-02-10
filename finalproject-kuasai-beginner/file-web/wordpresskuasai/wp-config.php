edc392873ba5183ee5dbfbf4a8eeae7938af9e69
Ahmad Muyassar <amuyassar16@gmail.com>
2021-02-10 14:51:16 +0000

    fix:wp-config

--- finalproject-kuasai-beginner/file-web/wordpresskuasai/wp-config.php
+++ finalproject-kuasai-beginner/file-web/wordpresskuasai/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'cilsykuasaiuser');
 define('DB_PASSWORD', 'cilsykuasaipassword');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql-kuasai');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
